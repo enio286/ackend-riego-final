d0158dc87f67d106bb311a546dd699906cc9474c
enio286 <enioandres28@gmail.com>
2026-06-12 23:05:57 -0500

    erro123

--- backend-riego-final/config/settings.py
+++ backend-riego-final/config/settings.py
@@ -182,7 +182,7 @@ MQTT_TOPIC_COMANDO = os.getenv(
 
 import os
 
-FRONTEND_URL = "http://localhost:5173"
+FRONTEND_URL = os.getenv("https://frontend-riegofinal-production.up.railway.app/", "http://localhost:5173")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
@@ -191,5 +191,3 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-
